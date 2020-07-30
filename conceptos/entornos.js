cba2359e895d7ec480458ec06e46b0e94a17a53f
Samuel Gonzalez <xame2005@gmail.com>
2020-07-29 23:18:39 -0500

    Ṕromesas y async await en node

--- conceptos/entornos.js
+++ conceptos/entornos.js
@@ -1,5 +1,7 @@
 const nombre = process.env.NOMBRE || "Sin nombre";
-const web = process.env.WEB || "No tengo Web"
+const web = process.env.WEB || "No tengo Web";
+const email = process.env.EMAIL || "sin correo electrónico"
 
 console.log("Hola " + nombre);
 console.log("Mi web es " + web);
+console.log("mi correo es " + email)
